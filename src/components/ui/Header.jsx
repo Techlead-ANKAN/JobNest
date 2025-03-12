4149b217353dc058659c0a3953000b14ae654b03
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-03-12 21:19:25 +0530

    updated header componenet

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,33 +1,92 @@
-// Dependencies
-import React from 'react'
-import { Link } from 'react-router-dom'
+// // Dependencies
+// import React from 'react'
+// import { Link } from 'react-router-dom'
 
-// Button - ShadCN Ui 
-import { Button } from './button'
+// // Button - ShadCN Ui 
+// import { Button } from './button'
 
-// Assests
-import Logo from "../../../public/logo.jpg"
+// // Assests
+// import Logo from "../../../public/logo.jpg"
 
+// import "./Header.css"
+
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+
+// function Header() {
+//   return (
+//     <nav className='py-4 flex justify-between item-center'>
+//       <Link to="/">
+//         <img src={Logo} className="h-20" alt="Logo" />
+//       </Link>
+
+//       {/* <Button variant="outline">Login</Button> */}
+
+//       <SignedOut>
+//         <SignInButton />
+//       </SignedOut>
+//       <SignedIn>
+//         <UserButton />
+//       </SignedIn>
+//     </nav>
+//   )
+// }
+
+// export default Header
+
+
+
+
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-
+import Logo from "../../../public/logo.png";
+import "./Header.css";
 
 function Header() {
   return (
-    <nav className='py-4 flex justify-between item-center'>
-      <Link to="/">
-        <img src={Logo} className="h-20" alt="Logo" />
-      </Link>
+    <header className="main-header">
+      <nav className="header-content">
+        <div className="brand-container">
+          <Link to="/" className="logo-link">
+            <img src={Logo} className="header_logo" alt="CareerConnect" />
+          </Link>
+        </div>
+        <h1 className="site-title">
+          <span className="typing-animation">JobNest</span>
+        </h1>
 
-      <Button variant="outline">Login</Button>
-
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
-    </nav>
-  )
+        <div className="auth-section">
+          <SignedOut>
+            <SignInButton mode='modal'>
+              <button className="sign-in-button">
+                Sign In
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <div className="user-profile">
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonTrigger: {
+                      width: "40px",  // New size
+                      height: "40px", // New size
+                    },
+                    userButtonPopoverCard: "user-popover",
+                    avatarBox: "user-avatar-box",
+                    avatarImage: "user-avatar-image"
+                  }
+                }}
+              />
+            </div>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
