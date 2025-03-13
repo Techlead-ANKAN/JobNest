1883fa62c66707720298f92540443498654350c6
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-03-13 13:56:53 +0530

    ///

---
+++ src/components/ui/SignInModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SignInModal = () => {
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Sign In to Continue</h2>
+        <p>Please sign in to post or find jobs.</p>
+      </div>
+    </div>
+  );
+};
+
+export default SignInModal;
