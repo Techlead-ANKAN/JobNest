377b6964cf3e077a5122dddcab4d1a397f403b2c
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-03-13 13:56:40 +0530

    ///

---
+++ src/components/ui/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react';
+
+function ProtectedRoute({children}) {
+
+    const {isSignedIn, user, isLoaded} = useUser();
+    const {pathname} = useLocation();
+
+    if(isLoaded && !isSignedIn && isSignedIn !==undefined){
+        return  <Navigate to="/sign-in=true" />;
+    }
+
+  return children;
+}
+
+export default ProtectedRoute
