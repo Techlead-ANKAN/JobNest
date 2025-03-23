fc9ef79eac294dc8f9907f3983b28904f6633a68
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-03-23 12:27:00 +0530

    added routes for apply page with dynamic job id and user id params

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import PostJobs from './pages/PostJobs'
 import SavedJobs from './pages/SavedJobs'
 import MyJobs from './pages/MyJobs'
 import ProtectedRoute from './components/ui/ProtectedRoute'
+import ApplyPage from './pages/ApplyPage'
 
 
 
@@ -73,6 +74,13 @@ const router = createBrowserRouter([
         element: 
         <ProtectedRoute>
           <MyJobs />
+        </ProtectedRoute>
+      },
+      {
+        path: "/apply/:jobId/:userId",
+        element:
+        <ProtectedRoute>
+          <ApplyPage />
         </ProtectedRoute>
       }
     ]
